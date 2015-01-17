ff3e34d57f2e22cfe7ac9393921c9a814fba3c9a
RobertZK <technoguyrob@gmail.com>
2015-01-16 23:29:01 -0600

    simplify

--- learn-you-a-haskell/typocracy.hs
+++ learn-you-a-haskell/typocracy.hs
@@ -7,5 +7,5 @@ lucky x = "Sorry, you're out of luck, pal!"
 
 factorial :: (Integral a) => a -> a
 factorial 0 = 1
-factorial 1 = 1
-factorial n = n * (factorial (n - 1))
+factorial n = n * factorial (n - 1)
+
