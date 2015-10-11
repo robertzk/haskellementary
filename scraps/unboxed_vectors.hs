0f2cbcb30b8c23c47cd1d96800bba407061ef1cc
RobertZK <technoguyrob@gmail.com>
2015-10-11 01:03:53 -0500

    Add link to docs for unboxed vectors

--- scraps/unboxed_vectors.hs
+++ scraps/unboxed_vectors.hs
@@ -1,7 +1,8 @@
+-- https://hackage.haskell.org/package/vector-0.11.0.0/docs/Data-Vector-Unboxed.html
 import Prelude (($), (*), print)
--- import Prelude qualified P
 import Data.Vector.Unboxed (toList, take, generate)
 
 main = do 
   let x = generate 100 (2 *)
   print $ toList $ take 2 x
+
